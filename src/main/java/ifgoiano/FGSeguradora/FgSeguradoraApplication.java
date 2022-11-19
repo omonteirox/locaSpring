3d8432a1e4008777688bbcb366295e67eb371914
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-11-19 16:21:34 -0300

    Apagando o exemplo de funcionamento das listas; Removendo importações não utilizadas

--- src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
+++ src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
@@ -5,15 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
 @SpringBootApplication
 public class FgSeguradoraApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FgSeguradoraApplication.class, args);
 
-
 	}
-
-
 }
