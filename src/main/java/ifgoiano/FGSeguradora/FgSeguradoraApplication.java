bd86fbe98b578a12a8eb328057f10b95f2facc77
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-11-19 15:59:07 -0300

    Apagando o exemplo de funcionamento das listas; Removendo importações não utilizadas

--- src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
+++ src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
@@ -1,14 +1,9 @@
 package ifgoiano.FGSeguradora;
 
-import ifgoiano.FGSeguradora.models.Automovel;
-import ifgoiano.FGSeguradora.models.Carro;
-import ifgoiano.FGSeguradora.models.Cliente;
-import ifgoiano.FGSeguradora.models.Moto;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class FgSeguradoraApplication {
