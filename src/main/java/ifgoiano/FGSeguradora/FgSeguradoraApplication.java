4c392d8477139f75bcd38f4ab749dab3d205211d
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-11-23 09:29:25 -0300

    teste

--- src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
+++ src/main/java/ifgoiano/FGSeguradora/FgSeguradoraApplication.java
@@ -3,7 +3,8 @@ package ifgoiano.FGSeguradora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 
 
 @SpringBootApplication
@@ -11,7 +12,6 @@ public class FgSeguradoraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FgSeguradoraApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 
 	}
 }
--- src/main/java/ifgoiano/FGSeguradora/repository/GerenteRepository.java
+++ src/main/java/ifgoiano/FGSeguradora/repository/GerenteRepository.java
@@ -3,8 +3,5 @@ package ifgoiano.FGSeguradora.repository;
 import ifgoiano.FGSeguradora.models.Gerente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface GerenteRepository extends JpaRepository<Gerente, Long> {
-    Optional<Gerente> findByUserName(String usuario);
 }
