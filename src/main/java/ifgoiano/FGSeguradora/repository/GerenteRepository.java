39938a61c507d74526531ed87fdc7c46051c41ae
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-11-23 07:39:01 -0300

    teste

--- src/main/java/ifgoiano/FGSeguradora/repository/GerenteRepository.java
+++
@@ -1,7 +0,0 @@
-package ifgoiano.FGSeguradora.repository;
-
-import ifgoiano.FGSeguradora.models.Gerente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GerenteRepository extends JpaRepository<Gerente, Long> {
-}
