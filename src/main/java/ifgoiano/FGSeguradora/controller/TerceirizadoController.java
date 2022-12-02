13583aaaa6d0882725d683d401838ef3d431b4ea
FelipinDaMassa <feliperibeiroaraujo3@hotmail.com>
2022-12-02 12:30:44 -0300

    Finalizado o MANY TO MANY
    feito em SERVIÇO e TERCEIRIZADO

--- src/main/java/ifgoiano/FGSeguradora/controller/TerceirizadoController.java
+++ src/main/java/ifgoiano/FGSeguradora/controller/TerceirizadoController.java
@@ -2,56 +2,62 @@ package ifgoiano.FGSeguradora.controller;
 
 import ifgoiano.FGSeguradora.DTO.TerceirizadoDTO;
 import ifgoiano.FGSeguradora.models.Terceirizado;
+import ifgoiano.FGSeguradora.repository.TerceirizadoRepository;
 import ifgoiano.FGSeguradora.service.TerceirizadoService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
+
+
 @Api(tags = "Terceirizado",description = "Tudo sobre Terceirizado")
 @RestController
 @RequestMapping("/terceirizado")
 public class TerceirizadoController {
     @Autowired
     public TerceirizadoService service;
-    @ApiOperation(value = "Consultar Terceirizado")
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<TerceirizadoDTO> findById(@PathVariable Long id) {
-        TerceirizadoDTO objDTO = new TerceirizadoDTO(service.findById(id));
-        return ResponseEntity.ok().body(objDTO);
-    }
-    @ApiOperation(value = "Criar Terceirizado")
-    @PostMapping
-    public ResponseEntity<TerceirizadoDTO> create(@Valid @RequestBody TerceirizadoDTO objDTO) {
-        Terceirizado newObj = service.create(objDTO);
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
-        return ResponseEntity.created(uri).build();
-    }
-    @ApiOperation(value = "Listar todos os Terceirizados")
-    @GetMapping
-    public ResponseEntity<List<TerceirizadoDTO>> findAll() {
-        List<TerceirizadoDTO> listDTO = service.findAll()
-                .stream().map(obj -> new TerceirizadoDTO(obj)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
-    }
-    @ApiOperation(value = "Atualizar Terceirizado")
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<TerceirizadoDTO> update(@PathVariable Long id, @Valid @RequestBody TerceirizadoDTO objDTO) {
-        TerceirizadoDTO newObj = new TerceirizadoDTO(service.update(id, objDTO));
-        return ResponseEntity.ok().body(newObj);
+
+    @Autowired
+    public TerceirizadoRepository repository;
+    @PostMapping("/create")
+
+    public ResponseEntity<Terceirizado> create(@RequestBody Terceirizado terceirizado) {
+        Terceirizado obj = service.create(terceirizado);
+        return ResponseEntity.ok().body(obj);
 
     }
-    @ApiOperation(value = "Deletar Terceirizado")
-    @DeleteMapping(value = "/{id}")
+
+    @GetMapping("/detalhes/{id}")
+
+    public TerceirizadoDTO findById(@PathVariable Long id) {
+        if(repository.findById(id).isPresent()) {
+            return service.findById(id);
+        }
+    else return  null;
+
+    }
+
+    @GetMapping("/all")
+
+    public List<TerceirizadoDTO> findAll() {
+        return service.findAll();
+    }
+
+    @PutMapping("/atualizar/{id}")
+
+    public Terceirizado update(@PathVariable Long id, @RequestBody Terceirizado terceirizado) {
+
+        return service.update(id,terceirizado);
+
+    }
+
+    @DeleteMapping("/deletar/{id}")
+
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+
     }
 }
--- src/main/java/ifgoiano/FGSeguradora/models/Terceirizado.java
+++ src/main/java/ifgoiano/FGSeguradora/models/Terceirizado.java
@@ -1,13 +1,25 @@
 package ifgoiano.FGSeguradora.models;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import ifgoiano.FGSeguradora.DTO.ServicoDTO;
+import ifgoiano.FGSeguradora.DTO.TerceirizadoDTO;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_terceirizado")
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Terceirizado {
 
     @Id
@@ -20,17 +32,19 @@ public class Terceirizado {
     @Column(nullable = false)
     private String telefone;
 
-    @Column(nullable = false, unique=true)
+    @Column(nullable = false, unique = true)
     private String cnpj;
 
-    //@ManyToMany
-    private String servico;
+    @ManyToMany(targetEntity = Servico.class, cascade = CascadeType.ALL)
+    private List<Servico> servicos;
 
-    public Terceirizado(Long id, String razaoSocial, String telefone, String cnpj, String servico) {
+    public Terceirizado(Long id, String razaoSocial, String telefone, String cnpj) {
         this.id = id;
         this.razaoSocial = razaoSocial;
         this.telefone = telefone;
         this.cnpj = cnpj;
-        this.servico = servico;
     }
+
+
 }
+
